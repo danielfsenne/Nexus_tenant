515ce44bd8d8ed4b78abd66a49442e0a878df13f
danielSenne <danielflausinosenne123@gmail.com>
2026-08-20 08:33:32 -0300

    configura Redis e Spring Cache

--- backend/src/main/java/com/nexus/backend/NexusBackendApplication.java
+++ backend/src/main/java/com/nexus/backend/NexusBackendApplication.java
@@ -2,8 +2,10 @@ package com.nexus.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class NexusBackendApplication {
 
 	public static void main(String[] args) {
